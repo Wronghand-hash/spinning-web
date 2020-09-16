7686e1ac36b6673c396aff439cfa9cc79160faad
Wronghand-hash <62405360+Wronghand-hash@users.noreply.github.com>
2020-09-16 18:10:50 +0330

    cards edited

--- static/script.js
+++ static/script.js
@@ -1,5 +1,3 @@
-let card = document.querySelector("#card");
-console.log(card);
-card.addEventListener('mouseover', () => {
-  document.body.style.backgroundImage = "{% static 'frontend.jpg' %}";
-});
+let cards = document.querySelectorAll('.col-lg')
+
+console.log(cards)
